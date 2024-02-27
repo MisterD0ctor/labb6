4aae21c2e91f4fdedc8207e584430d0cfb719140
kaspe <kaspe@DESKTOP-4VPBI13>
2024-02-28 00:58:23 +0100

    Added CloseEvent

---
+++ labb6/src/supermarket_simulator/events/CloseEvent.java
@@ -0,0 +1,20 @@
+package supermarket_simulator.events;
+import generic_simulator.Event;
+import generic_simulator.EventQueue;
+import generic_simulator.State;
+import supermarket_simulator.StoreState;
+
+public class CloseEvent extends Event {
+
+	public CloseEvent(double time) {
+		super(time);
+	}
+	
+	@Override
+	public void execute(State state, EventQueue eventQueue) {
+		super.execute(state, eventQueue);
+		
+		StoreState s = (StoreState)state;
+		s.isOpen = false;
+	}
+}
