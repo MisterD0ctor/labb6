7c5a70cac903062593d2e77ac2ffe97d2d6d2ea2
kaspe <kaspe@DESKTOP-4VPBI13>
2024-02-28 00:48:06 +0100

    Added stuff

--- labb6/src/supermarket_simulator/RunSim.java
+++ labb6/src/supermarket_simulator/RunSim.java
@@ -1,8 +1,25 @@
 package supermarket_simulator;
 
 public class RunSim {
-
+	
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
+		
+		StoreState state = new StoreState(
+				Integer.parseInt(args[0]),
+				Integer.parseInt(args[1]),
+				Double.parseDouble(args[2]),
+				Double.parseDouble(args[3]),
+				Double.parseDouble(args[4]),
+				Double.parseDouble(args[5]),
+				Double.parseDouble(args[6]),
+				Integer.parseInt(args[7]));
+		
+		StoreView view = new StoreView();
+		
+		state.addObserver(view);
+		
+		
 		
 	}
 
