ac64f51222e2a609ecc3dfc498824a44453ca701
kaspe <kaspe@DESKTOP-4VPBI13>
2024-02-27 22:09:39 +0100

    better variable names

--- labb6/src/supermarket_simulator/StoreState.java
+++ labb6/src/supermarket_simulator/StoreState.java
@@ -4,8 +4,11 @@ import generic_simulator.State;
 class StoreState extends State {
 	public boolean isOpen;
 	
-	private int n;
-	private int m;
+	final public int cashierCount;
+	final public int maxCustomerCount;
 	
-	
+	public StoreState(int n, int m, double lambda, double kMin, double kMax, double pMin, double pMax, int f) {
+		cashierCount = n;
+		maxCustomerCount = m;
+	}
 }
