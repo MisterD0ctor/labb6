50c7897f5c803ad36a518272a6228858e259e080
kaspe <kaspe@DESKTOP-4VPBI13>
2024-02-27 22:10:16 +0100

    added StoreView

---
+++ labb6/src/supermarket_simulator/StoreView.java
@@ -0,0 +1,13 @@
+package supermarket_simulator;
+import java.util.Observable;
+
+import generic_simulator.View;
+
+@SuppressWarnings("deprecation")
+class StoreView extends View {
+	
+	@Override
+	public void update(Observable o, Object arg) {
+
+	}
+}
