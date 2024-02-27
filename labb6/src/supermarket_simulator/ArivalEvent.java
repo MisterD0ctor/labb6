d06a94ee071e1c0d03cf0b9a3a2aa49583338110
kaspe <kaspe@DESKTOP-4VPBI13>
2024-02-27 22:07:46 +0100

    Added todo

--- labb6/src/supermarket_simulator/ArivalEvent.java
+++ labb6/src/supermarket_simulator/ArivalEvent.java
@@ -12,6 +12,23 @@ class ArivalEvent extends Event {
 	public void execute(State state, EventQueue eventQueue) {
 		super.execute(state, eventQueue);
 		
-		eventQueue.enqueue(null);
+		/* TODO
+		 * En kund C som anländer släpps in endast om snabbköpet ännu är öppet och det finns
+         * plats. Släpps C in är det en till kund inne i snabbköpet som kommer att handa varor.
+         * 
+		 * Om det är öppet men fullt så avviker C istället utan att handla och har vi missat en kund.
+         * En kund som anländer efter stängning ska däremot inte räknas som en missad kund.
+         * 
+         * Först ska C plocka ihop sina varor. Vi skapar en framtida plockhändelse för C (se nedan)
+         * som inträffar när C plockat klart och gått bort till kassorna. Denna läggs till händelsekön.
+         * Under förutsättning att snabbköpet har öppet ska slutligen också en (1) ny framtida
+         * ankomsthändelse (för en ny kund) skapas och lagras i händelsekön.
+         * 
+         * Att varje ankomsthändelse genererar nästa gör att vi dels vet att det alltid finns en
+         * ankomsthändelse i kön dels minskar behovet av minne som kön behöver. Sämre vore att
+         * generera alla framtida ankomsthändelser innan simuleringen startar, något som alltså
+         * inte ska göras.
+		 */
+		
 	}
 }
