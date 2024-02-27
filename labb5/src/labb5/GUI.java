ad24a6ed2f292ce36d124630774979ea7f1dabc8
kaspe <kaspe@DESKTOP-4VPBI13>
2024-02-27 12:34:14 +0100

    fixa i gui

--- labb5/src/labb5/GUI.java
+++ labb5/src/labb5/GUI.java
@@ -5,12 +5,11 @@ import javax.swing.JFrame;
 
 public class GUI extends JFrame{
 	public static void main(String args[]){
-	       JFrame frame = new JFrame("My First GUI");
-	       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	       frame.setSize(300,300);
-	       JButton button = new JButton("Press");
-	       frame.getContentPane().add(button); // Adds Button to content pane of frame
-	       frame.setVisible(true);
-	       
-	    }
+		JFrame frame = new JFrame("My First GUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,300);
+        JButton button = new JButton("Press");
+        frame.getContentPane().add(button); // Adds Button to content pane of frame
+        frame.setVisible(true);   
+	}
 }
