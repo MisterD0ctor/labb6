a0e31496b972bc27e9ef3c58361fd8afd44ab51a
kaspe <kaspe@DESKTOP-4VPBI13>
2024-02-28 01:25:20 +0100

    Added pickTime and payTime

--- labb6/src/supermarket_simulator/StoreState.java
+++ labb6/src/supermarket_simulator/StoreState.java
@@ -8,13 +8,18 @@ public class StoreState extends State {
 	public final int cashierCount;
 	public final int maxCustomerCount;
 	
-	public final ArivalTimeProvider arivalTime;
+	public final ExponentialTimeProvider arivalTime;
+	public final UniformTimeProvider pickTime;
+	public final UniformTimeProvider payTime;
 	
 	public StoreState(int n, int m, double lambda, 
 			double kMin, double kMax, double pMin, 
 			double pMax, long f) {
+		
 		cashierCount = n;
 		maxCustomerCount = m;
-		arivalTime = new ArivalTimeProvider(this, lambda, f);
+		arivalTime = new ExponentialTimeProvider(this, lambda, f);
+		pickTime = new UniformTimeProvider(this, kMin, kMax, f);
+		payTime = new UniformTimeProvider(this, pMin, pMax, f);
 	}
 }
