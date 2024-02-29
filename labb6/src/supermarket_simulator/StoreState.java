11efcbb103b1f2f6077d6d228fd76f86e99e2183
kaspe <kaspe@DESKTOP-4VPBI13>
2024-02-29 15:17:22 +0100

    StoreState updaterad

--- labb6/src/supermarket_simulator/StoreState.java
+++ labb6/src/supermarket_simulator/StoreState.java
@@ -3,17 +3,20 @@ import generic_simulator.State;
 import supermarket_simulator.time.*;
 
 public class StoreState extends State {
-	public boolean isOpen;
+	public boolean isClosed;
 	
-	public final int cashierCount;
+	public final int checkoutsCount;
 	public final int maxCustomerCount;
 	
-	public int availableCashierCount;
+	public int availableCheckoutsCount;
 	public int customerCount;
 	public int payCount;
 	public int missedCustomerCount;
-	public double cashierInactivityTime;
+	public double checkoutsInactivityTime;
 	public double totalQueueTime;
+	
+	public final CustomerFactory customerFactory;
+	public FIFO<Customer> checkoutQueue;
 	
 	public final ExponentialTimeProvider arivalTime;
 	public final UniformTimeProvider pickTime;
@@ -23,17 +26,21 @@ public class StoreState extends State {
 			double kMin, double kMax, double pMin, 
 			double pMax, long f) {
 		
-		cashierCount = n;
+		checkoutsCount = n;
 		maxCustomerCount = m;
 		arivalTime = new ExponentialTimeProvider(this, lambda, f);
 		pickTime = new UniformTimeProvider(this, kMin, kMax, f);
 		payTime = new UniformTimeProvider(this, pMin, pMax, f);
 		
-		availableCashierCount = 0;
+		isClosed = false;
+		availableCheckoutsCount = 0;
 		customerCount = 0;
 		payCount = 0;
 		missedCustomerCount = 0;
-		cashierInactivityTime = 0;
+		checkoutsInactivityTime = 0;
 		totalQueueTime = 0;
+		
+		customerFactory = new CustomerFactory();
+		checkoutQueue = new FIFO<Customer>();
 	}
 }
