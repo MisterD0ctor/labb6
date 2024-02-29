da91dca7c6c9c79f0211731781c859c0959c5208
kaspe <kaspe@DESKTOP-4VPBI13>
2024-02-29 14:20:08 +0100

    added more state variables

--- labb6/src/supermarket_simulator/StoreState.java
+++ labb6/src/supermarket_simulator/StoreState.java
@@ -8,6 +8,8 @@ public class StoreState extends State {
 	public final int cashierCount;
 	public final int maxCustomerCount;
 	
+	public int availableCashierCount;
+	public int customerCount;
 	public int payCount;
 	public int missedCustomerCount;
 	public double cashierInactivityTime;
@@ -16,8 +18,6 @@ public class StoreState extends State {
 	public final ExponentialTimeProvider arivalTime;
 	public final UniformTimeProvider pickTime;
 	public final UniformTimeProvider payTime;
-	
-	
 	
 	public StoreState(int n, int m, double lambda, 
 			double kMin, double kMax, double pMin, 
@@ -28,5 +28,12 @@ public class StoreState extends State {
 		arivalTime = new ExponentialTimeProvider(this, lambda, f);
 		pickTime = new UniformTimeProvider(this, kMin, kMax, f);
 		payTime = new UniformTimeProvider(this, pMin, pMax, f);
+		
+		availableCashierCount = 0;
+		customerCount = 0;
+		payCount = 0;
+		missedCustomerCount = 0;
+		cashierInactivityTime = 0;
+		totalQueueTime = 0;
 	}
 }
