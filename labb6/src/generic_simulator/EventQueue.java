f919fdae65cdfba73c83c9d3f0379ff303e7736e
david <david@StellarComputer>
2024-02-29 14:33:53 +0100

    EventQueue Kanske färdig!

--- labb6/src/generic_simulator/EventQueue.java
+++ labb6/src/generic_simulator/EventQueue.java
@@ -1,14 +1,27 @@
 package generic_simulator;
 
+import java.util.PriorityQueue; //TILLAGD
+
+
 public class EventQueue {
+
+	private PriorityQueue<Event> queue; // En prioritetskö för händelser
 	
+	public EventQueue() {
+        queue = new PriorityQueue<>((e1, e2) -> Double.compare(e1.time, e2.time));
+    }
+		
 	public void enqueue(Event event) {
-		// TODO: implement enqueue
+		queue.offer(event); // Lägg till händelsen i köen	
 	}
 	
 	Event dequeue() {
-		// TODO: implement dequeue
-		return null;
+		return queue.poll(); // Ta bort och returnera den första händelsen i köen //Ifall tom så returnerar den null
 	}
+	
+	//NYTT HELA VÄGEN NER FÖR ATT SE IFALL DET ÄR TOMT
+	public boolean isEmpty() {
+        return queue.isEmpty(); // Kontrollera om köen är tom
+    }
 }
 //DAVID TAR DENNA
