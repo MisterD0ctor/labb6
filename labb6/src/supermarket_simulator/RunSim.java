b7bdf33cddc469fae100d2bab3f456faf9ac2985
kaspe <kaspe@DESKTOP-4VPBI13>
2024-02-29 13:41:36 +0100

    Added StopEvent

--- labb6/src/supermarket_simulator/RunSim.java
+++ labb6/src/supermarket_simulator/RunSim.java
@@ -1,4 +1,6 @@
 package supermarket_simulator;
+import generic_simulator.*;
+import supermarket_simulator.events.*;
 
 public class RunSim {
 	
@@ -13,14 +15,16 @@ public class RunSim {
 				Double.parseDouble(args[4]),
 				Double.parseDouble(args[5]),
 				Double.parseDouble(args[6]),
-				Integer.parseInt(args[7]));
+				Integer.parseInt(args[8]));
 		
 		StoreView view = new StoreView();
-		
 		state.addObserver(view);
-		
-		
-		
+		EventQueue eventQueue = new EventQueue();
+		eventQueue.enqueue(new StartEvent());
+		eventQueue.enqueue(new CloseEvent(Double.parseDouble(args[7])));
+		eventQueue.enqueue(new StopEvent(999.0));
+		Simulator sim = new Simulator(state, eventQueue);
+		sim.run();
 	}
 
 }
