86944bfca17672242ea9306105eadde2ef49aec5
david <david@StellarComputer>
2024-03-02 18:34:40 +0100

    Kommenterat färdigt i RunSim

--- labb6/src/supermarket_simulator/RunSim.java
+++ labb6/src/supermarket_simulator/RunSim.java
@@ -6,22 +6,22 @@ import supermarket_simulator.events.*;
 public class RunSim {
 
 	@SuppressWarnings("deprecation")
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 
-		int openCheckouts = Integer.parseInt(args[1]);
-		int customerCapacity = Integer.parseInt(args[1]);
-		double arivalFrequency = Double.parseDouble(args[2]);
-		double minPickTime = Double.parseDouble(args[3]);
-		double maxPickTime = Double.parseDouble(args[4]);
-		double minPayTime = Double.parseDouble(args[5]);
+		int openCheckouts = Integer.parseInt(args[1]); //Skapar antalet kassor, ändrar om args till int.
+		int customerCapacity = Integer.parseInt(args[1]); //Hur många som får finnas i butiken
+		double arivalFrequency = Double.parseDouble(args[2]); //Hur ofta nya kunder kommer, medel för frekvensen över hur många som går in i butiken... Ifall arrivalfrequency är 4 kommer en costumer per kvart
+		double minPickTime = Double.parseDouble(args[3]); //Lägsta tiden det tar för en kund att plocka ihop sina varor
+		double maxPickTime = Double.parseDouble(args[4]); 
+		double minPayTime = Double.parseDouble(args[5]); //Lägsta tiden det tar för en kund att betala för sina varor
 		double maxPayTime = Double.parseDouble(args[6]);
-		long seed = Integer.parseInt(args[8]);
-
+		long seed = Long.parseLong(args[8]); //Fröet som bestämmer hur slumptalen ska genereras, alla argument kommer i en array av åtta element
+		
 		SupermarketState state = new SupermarketState(openCheckouts, customerCapacity, arivalFrequency, minPickTime,
-				maxPickTime, minPayTime, maxPayTime, seed);
-
+				maxPickTime, minPayTime, maxPayTime, seed); // Skickar med alla parametrar till state
+		
 		SupermarketView view = new SupermarketView(openCheckouts, customerCapacity, arivalFrequency, minPickTime,
-				maxPickTime, minPayTime, maxPayTime, seed);
+				maxPickTime, minPayTime, maxPayTime, seed); // Skickar med alla parametrar till View så att de kan skrivas ut i konsolen
 		
 		state.addObserver(view); // Lägger view som observer på Supermarket
 		EventQueue eventQueue = new EventQueue(); //Skapar ny eventqueue
