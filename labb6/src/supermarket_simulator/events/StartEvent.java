31c94321ef7bc0ef30384e44ce569a48ea4b18bd
david <david@StellarComputer>
2024-03-02 18:57:46 +0100

    StartEvent Färdig kommenterad!

--- labb6/src/supermarket_simulator/events/StartEvent.java
+++ labb6/src/supermarket_simulator/events/StartEvent.java
@@ -4,17 +4,17 @@ import generic_simulator.EventQueue;
 import generic_simulator.State;
 
 public class StartEvent extends SupermarketEvent {
-	
+
 	public StartEvent() {
-		super(0);
+		super(0); // Super den kallar på konstruktorn i överklassen, Skapar ett event med tiden
+					// noll.
 	}
-	
+
 	@Override
 	public void execute(State state, EventQueue eventQueue) {
-		super.execute(state, eventQueue);
-				
-		eventQueue.enqueue(new ArivalEvent(
-				store.nextArivalTime(), 
-				store.getCustomer()));
+		super.execute(state, eventQueue); // Kör execute från SupermarketEvent
+
+		eventQueue.enqueue(new ArivalEvent(store.nextArivalTime(), store.getCustomer())); //Skapar ett nytt arivalevent och vi hämtar tiden de eventet ska ske, 
+		//sen skapar vi också en ny kund som de eventet ska ske för. Skickar vidare de eventet till eventkön
 	}
 }
