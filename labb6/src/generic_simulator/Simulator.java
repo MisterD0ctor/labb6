789b11c77900e1df655a49854772f54a30e34688
david <david@StellarComputer>
2024-03-02 18:42:37 +0100

    Simulator är Färdig kommenterat!

--- labb6/src/generic_simulator/Simulator.java
+++ labb6/src/generic_simulator/Simulator.java
@@ -1,20 +1,20 @@
 package generic_simulator;
 
 public class Simulator {
-	private State state;
-	private EventQueue eventQueue;
+	private State state; //Tillståndet som simuleringen körs på
+	private EventQueue eventQueue; //Event kön som innehåller alla event
 	
 	public Simulator(State state, EventQueue eventQueue) {
-		this.state = state;
-		this.eventQueue = eventQueue;
+		this.state = state; // För att hålla koll på vilket state det är...
+		this.eventQueue = eventQueue; // För att vi vill kunna välja vilka event som ska finnas i eventkön från början
 	}
 	
-	public void run() {
+	public void run() { 
 		while (state.running) {
-			Event event = eventQueue.dequeue();
+			Event event = eventQueue.dequeue(); //plockar event från eventqueue via dequeue för att sedan köra dem
 			
 			if (event != null) {
-				event.execute(state, eventQueue);				
+				event.execute(state, eventQueue); //Ifall inte null, körs eventet				
 			}
 		}
 	}
