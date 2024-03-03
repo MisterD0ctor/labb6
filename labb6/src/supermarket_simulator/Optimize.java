86f8ee12f1e26ea2b87919126b87d9afc33734ee
ludvigpernskold <ludvigpernskold@pub108-51.pub.ltu.se>
2024-03-03 19:14:59 +0100

    OPTIMIZER

--- labb6/src/supermarket_simulator/Optimize.java
+++ labb6/src/supermarket_simulator/Optimize.java
@@ -13,56 +13,56 @@ public class Optimize {
 
 	public static void main (String[] args) {
 		Optimize op = new Optimize(); 
-		System.out.print(op.methodtwo());
+		System.out.print(op.optimalCheckouts(5, 1, 0.5, 1.0, 2.0, 3.0,10,999, 1234));
 	}
 	
-	public SupermarketState runSim(int n, int m, double lambda, double kMin, double kMax, double pMin, double pMax,
-			long d, double s) { //Tar in 
+	public SupermarketState runSim(int checkouts, int customerCapacity, double arivalFrequency, double minPickTime,
+			double maxPickTime, double minPayTime, double maxPayTime,double closeTime,double stopTime, long seed) { //Tar in 
 		
-		SupermarketState state = new SupermarketState(n, m, lambda, kMin, kMax, pMin, pMax, d);
+		SupermarketState state = new SupermarketState( checkouts,  customerCapacity, arivalFrequency, minPickTime,
+			 maxPickTime, minPayTime, maxPayTime, seed);
 		//
 		
 		EventQueue eventQueue = new EventQueue(); //Skapar ny instans av klassen EventQueue
 		eventQueue.enqueue(new StartEvent(0));
-		eventQueue.enqueue(new CloseEvent(s));
-		eventQueue.enqueue(new StopEvent(999.0));
+		eventQueue.enqueue(new CloseEvent(closeTime));
+		eventQueue.enqueue(new StopEvent(stopTime));
 		Simulator sim = new Simulator(state, eventQueue); //Skickar med det state som simuleringen skall utföras på och eventQueue (kön) som den skall innehålla
 		sim.run();
 		return state;
 	}
 
+	public int optimalCheckouts( int customerCapacity, double arivalFrequency, double minPickTime,
+			double maxPickTime, double minPayTime, double maxPayTime,double closeTime,double stopTime, long seed) {
+		int i = customerCapacity;
+		int optimalMissed = runSim(i, customerCapacity, arivalFrequency, minPickTime,maxPickTime, minPayTime, maxPayTime,closeTime,stopTime, seed).missedCustomers();;
+	    int step = i -1;
+	    int currentMissed;
 	
-	//Metod 2 SAKNAR BINÄRSÖKNING
-	/*
-	public int methodtwo () {
-		int miss = 1;
-		int i;
-		for (i = 1; miss > 0; i++){
-			miss = runSim(i, 5, 1.0, 2.0, 3.0, 0.5, 1.0, 1234, 16.0).missedCustomers();
-		}
-		return i;
-	}*/
-	public int methodtwo() {
-	    int miss = 1;
-	    int i = 1;  // Starta med 1 eftersom du inte kan ha 0 kassor.
-	    while (miss > 0) {
-	        // Kör simuleringen med 'i' kassor.
-	        miss = runSim(i, 7, 2.0, 2.0, 3.0, 0.5, 1.0, 1234, 16.0).missedCustomers();
-	        i++;  // Öka antalet kassor för nästa iteration.
+	    while(step > 0) {
+	    	step /= 2;
+	    	i -= step;
+	    	currentMissed = runSim(i, customerCapacity, arivalFrequency, minPickTime,maxPickTime, minPayTime, maxPayTime,closeTime,stopTime, seed).missedCustomers();
+	    	if(currentMissed > optimalMissed) {
+	    		i += step;
+	    	}
 	    }
-	    return i + 1;  // Returnera 'i - 1' eftersom 'i' har ökat även när miss = 0.
+	    return i;
 	}
+	    	
 	
 	
 	//Metod 3 - ska starta
 	
-	public int methodthree (int f) {
+	public int methodthree (int customerCapacity, double arivalFrequency, double minPickTime,
+		double maxPickTime, double minPayTime, double maxPayTime,double closeTime,double stopTime, int f) {
+		
 		Random random = new Random(f);
         int highestMin = Integer.MIN_VALUE; // Initial highest minimum value
         int consecutiveStableCount = 0;
 		
         while (consecutiveStableCount < 100) { // Loop until 100 consecutive stable iterations
-            int min = methodtwo(); // Run methodtwo
+            int min = optimalCheckouts( customerCapacity, arivalFrequency, minPickTime,maxPickTime, minPayTime, maxPayTime,closeTime,stopTime, f); // Run methodtwo
             if (min > highestMin) { // Check if new highest minimum found
                 highestMin = min;
                 consecutiveStableCount = 0; // Reset consecutive stable count
@@ -77,5 +77,3 @@ public class Optimize {
 	
 }
 
-
-//David TAR DENNA*/
