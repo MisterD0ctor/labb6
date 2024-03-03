f3e1e02f268da8c21a96fa7c4537264fa58149fe
david <david@StellarComputer>
2024-03-03 10:25:04 +0100

    Kommentarer påbörjar Optimize

--- labb6/src/supermarket_simulator/Optimize.java
+++ labb6/src/supermarket_simulator/Optimize.java
@@ -9,25 +9,17 @@ import supermarket_simulator.events.StartEvent;
 public class Optimize {
 
 	
-	public SupermarketState runSim(int n, int m, double lambda, 
-		double kMin, double kMax, double pMin, 
-		double pMax, long f, double s) {
+	public SupermarketState runSim(int n, int m, double lambda, double kMin, double kMax, double pMin, double pMax,
+			long f, double s) { //Tar in 
 		
-		SupermarketState state = new SupermarketState(
-				n, 
-				m,
-				lambda,
-				kMin,
-				kMax,
-				pMin,
-				pMax,
-				f);
+		SupermarketState state = new SupermarketState(n, m, lambda, kMin, kMax, pMin, pMax, f);
+		//
 		
-		EventQueue eventQueue = new EventQueue();
+		EventQueue eventQueue = new EventQueue(); //Skapar ny instans av klassen EventQueue
 		eventQueue.enqueue(new StartEvent());
 		eventQueue.enqueue(new CloseEvent(s));
 		eventQueue.enqueue(new StopEvent(999.0));
-		Simulator sim = new Simulator(state, eventQueue);
+		Simulator sim = new Simulator(state, eventQueue); //Skickar med det state som simuleringen skall utföras på och eventQueue (kön) som den skall innehålla
 		sim.run();
 		return state;
 	}
