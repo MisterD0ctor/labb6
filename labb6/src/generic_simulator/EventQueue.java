d9c565e0d77e4f150863f76bc402877212a1b525
kaspe <kaspe@DESKTOP-4VPBI13>
2024-03-03 11:30:27 +0100

    whitespace i EventQueue

--- labb6/src/generic_simulator/EventQueue.java
+++ labb6/src/generic_simulator/EventQueue.java
@@ -2,26 +2,23 @@ package generic_simulator;
 
 import java.util.PriorityQueue; //TILLAGD
 
-
 public class EventQueue {
 
 	private PriorityQueue<Event> queue; // En prioritetskö för händelser
 	
 	public EventQueue() {
-        queue = new PriorityQueue<>((e1, e2) -> Double.compare(e1.time, e2.time));
+        queue = new PriorityQueue<Event>((e1, e2) -> Double.compare(e1.time, e2.time));
     }
-		
+	
 	public void enqueue(Event event) {
-		queue.offer(event); // Lägg till händelsen i köen	
+		queue.offer(event); // Lägg till händelsen i kön	
 	}
 	
 	Event dequeue() {
-		return queue.poll(); // Ta bort och returnera den första händelsen i köen //Ifall tom så returnerar den null
+		return queue.poll(); // Ta bort och returnera den första händelsen i kön //Ifall tom så returnerar den null
 	}
 	
-	//NYTT HELA VÄGEN NER FÖR ATT SE IFALL DET ÄR TOMT
 	public boolean isEmpty() {
-        return queue.isEmpty(); // Kontrollera om köen är tom
+        return queue.isEmpty(); // Kontrollera om kön är tom
     }
 }
-//DAVID TAR DENNA
